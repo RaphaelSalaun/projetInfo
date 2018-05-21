2636c5902f734cbd10b3464d7437ed666959fd9f
RaphaelSalaun <39302315+RaphaelSalaun@users.noreply.github.com>
2018-05-22 01:20:10 +0200

    Add files via upload

---
+++ retestsocket/Client.java
@@ -0,0 +1,87 @@
+package com.example.xx_laphoune_xx.retestsocket;
+
+/**
+ * Created by Xx_LaPhoune_xX on 22/05/2018.
+ */
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client extends AsyncTask<Void, Void, String> {
+
+    String dstAddress;
+    int dstPort;
+    String response = "";
+    TextView textResponse;
+
+    private Socket connexion = null;
+    private PrintWriter writer = null;
+    private BufferedInputStream reader = null;
+
+    Client(String addr, int port, TextView textResponse) {
+        dstAddress = addr;
+        dstPort = port;
+        this.textResponse = textResponse;
+    }
+
+
+    @Override
+    protected String doInBackground(Void... arg0) {
+
+        Socket socket = null;
+
+        try {
+            socket = new Socket(dstAddress, dstPort);
+            writer = new PrintWriter(socket.getOutputStream(), true);
+            reader = new BufferedInputStream(socket.getInputStream());
+            //On envoie les donnÃ©es au serveur
+
+            String donnees = "Nikita Belluci Femme données";
+            writer.write(donnees);
+            writer.flush();
+            // System.out.println("Données envoyées au serveur");
+            read();
+        } catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            response = "UnknownHostException: " + e.toString();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            response = "IOException: " + e.toString();
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+        return response;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        textResponse.setText(response);
+        super.onPostExecute(result);
+    }
+
+    private String read() throws IOException{
+        String response = "";
+        int stream;
+        byte[] b = new byte[4096];
+        stream = reader.read(b);
+        response = new String(b, 0, stream);
+        return response;
+    }
+}
